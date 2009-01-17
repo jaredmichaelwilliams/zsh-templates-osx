30c856beeecadc4865cc073559b45f3b77f19713
wgscott1@mac.com <wgscott1@mac.com@da9ad0b7-883f-0410-87c7-35f673e70b47>
2009-01-17 00:08:54 +0000

    convert tabs to spaces for cleaner formatting

--- trunk/Library/init/zsh/zshrc.d/200_environment.local.legacy.zsh
+++ trunk/Library/init/zsh/zshrc.d/200_environment.local.legacy.zsh
@@ -4,21 +4,21 @@
 
 
 
-	if [[ $(uname) == Darwin ]];then
-	    ZDOT_USER="$HOME/Library/init/zsh"
-	elif [[ $(uname) == Linux ]];then   
-	    ZDOT_USER='~/.zsh'
-	elif [[ $(uname) == FreeBSD ]];then
-	    ZDOT_USER='~/.zsh'
-	fi
+    if [[ $(uname) == Darwin ]];then
+        ZDOT_USER="$HOME/Library/init/zsh"
+    elif [[ $(uname) == Linux ]];then   
+        ZDOT_USER='~/.zsh'
+    elif [[ $(uname) == FreeBSD ]];then
+        ZDOT_USER='~/.zsh'
+    fi
 
 
-	# Only attempt to source files from those directories that really exist.
-	# If $ZDOT and $ZDOT_USER are the same directory, we want to avoid doing 
-	# this twice.
+    # Only attempt to source files from those directories that really exist.
+    # If $ZDOT and $ZDOT_USER are the same directory, we want to avoid doing 
+    # this twice.
 
-	if [[ -d $ZDOT_USER && $ZDOT_USER != $ZDOT ]];then
-	 	foreach myconfigfile in $ZDOT_USER/*
-		    source $myconfigfile
-		end
+    if [[ -d $ZDOT_USER && $ZDOT_USER != $ZDOT ]];then
+        foreach myconfigfile in $ZDOT_USER/*
+            source $myconfigfile
+        end
     fi
