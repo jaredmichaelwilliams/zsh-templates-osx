70b01361b10fdf5a132cc1579cb38d5a9e3988ac
wgscott1@mac.com <wgscott1@mac.com@da9ad0b7-883f-0410-87c7-35f673e70b47>
2009-08-26 02:16:24 +0000

    further size adjustment to accomidate 50px menu bar

--- trunk/Library/init/zsh/zshrc.d/local-functions/etc/dotfiles/coot_zalman.scm
+++ trunk/Library/init/zsh/zshrc.d/local-functions/etc/dotfiles/coot_zalman.scm
@@ -11,10 +11,11 @@
     ;; These should be even numbers to keep the stereo
     ;; the right way around
         (set-graphics-window-position 0 0)
-        (set-graphics-window-size 1680 1020)
+        (set-graphics-window-size 1680 1000)
     ;; Make these appear on the right-hand monitor
         (set-model-fit-refine-dialog-position 1900 0)
         (set-delete-dialog-position 1700 50)
         (set-go-to-atom-window-position 1847 67)
 
  
+
