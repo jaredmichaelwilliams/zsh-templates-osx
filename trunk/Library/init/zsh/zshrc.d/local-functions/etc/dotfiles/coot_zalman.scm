0510e5e3073cc9e5437eb40c4dd29d55879f692b
wgscott1@mac.com <wgscott1@mac.com@da9ad0b7-883f-0410-87c7-35f673e70b47>
2009-08-26 01:03:15 +0000

    tweaked size of zalman full-screen to allow for menu bar

--- trunk/Library/init/zsh/zshrc.d/local-functions/etc/dotfiles/coot_zalman.scm
+++ trunk/Library/init/zsh/zshrc.d/local-functions/etc/dotfiles/coot_zalman.scm
@@ -11,7 +11,7 @@
     ;; These should be even numbers to keep the stereo
     ;; the right way around
         (set-graphics-window-position 0 0)
-        (set-graphics-window-size 1680 1050)
+        (set-graphics-window-size 1680 1020)
     ;; Make these appear on the right-hand monitor
         (set-model-fit-refine-dialog-position 1900 0)
         (set-delete-dialog-position 1700 50)
